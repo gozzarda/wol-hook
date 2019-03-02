f9db297dd6a526c5547e50d327f911253409acde
Andrew Gozzard <gozzardam@gmail.com>
2019-03-03 00:13:08 +0800

    Add a runtime argument for port number

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
+import System.Console.CmdArgs
 import Network.Wai.Handler.Warp (run)
 
 import WOLHook (app)
 
+data Opts = Opts {port :: Int} deriving (Show, Data, Typeable)
+
+opts = Opts {port = 8081 &= help "Port to listen on"}
+
 main :: IO ()
-main = run 8081 app
+main = do
+  args <- cmdArgs opts
+  run (port args) app
